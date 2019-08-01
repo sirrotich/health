5c5a8c5de3c43be5b058073af210a29d6ba4b9df
sirrotich <rotichtitus12@gmail.com>
2019-08-01 11:52:53 +0300

    main activity java

--- app/src/main/java/com/example/health/MainActivity.java
+++ app/src/main/java/com/example/health/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
